7d9927285892ef41be4fe7d727e6b180f34d4106
Michal Kokocinski <michal@waat.eu>
2022-02-02 17:42:11 +0100

    Convert Accordion to functional component.

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,36 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 import './style.sass'
 
+import Icon from '../Icon'
+import PropTypes from 'prop-types'
+import React from 'react'
+import classNames from 'classnames'
 
-class Accordion extends React.Component {
-  state = {
-    openedItem: false,
-  }
-  handleClick(index) {
-    this.setState({ openedItem: this.state.openedItem === index ? false : index })
-  }
-  render() {
-    const { openedItem } = this.state
-    const { children } = this.props
-    return (
-      <div className='accordion'>
-        {Array.isArray(children) && children.map((child, index) => ({
-          ...child,
-          props: {
-            ...child.props,
-            openedItem,
-            index,
-            handleClick: () => this.handleClick(index),
-          },
-        }))}
-      </div>
-    )
-  }
+const AccordionContext = React.createContext([null, () => {}])
+
+
+const Accordion = ({ children, boxed }) => {
+  const [openedItem, setOpenedItem] = React.useState(false)
+
+  return (
+    <AccordionContext.Provider value={[openedItem, setOpenedItem]}>
+      <div className={classNames('accordion', {
+        'accordion--boxed': boxed,
+      })}>{children}</div>
+    </AccordionContext.Provider>
+  )
 }
 
 Accordion.propTypes = {
   children: PropTypes.arrayOf(PropTypes.node).isRequired,
+  boxed: PropTypes.bool,
 }
 
 export default Accordion
@@ -38,22 +30,33 @@ export default Accordion
 
 export const AccordionItem = ({
   title,
+  className,
   children,
-  openedItem,
-  index,
-  handleClick,
-}) =>
-  <div
-    className={`accordion__item${openedItem === index ? ' is-expanded' : ''}`}
-    key={index}
-  >
-    <div className='accordion__title' onClick={() => handleClick()}>{title}</div>
-    {openedItem === index &&
-      <div className='accordion__content'>{children}</div>
-    }
-  </div>
+}) => {
+  const [openedItem, setOpenedItem] = React.useContext(AccordionContext)
+  const index = title // Use title as unique index.
+  const isOpen = openedItem === index
+
+  const handleClick = () => {
+    setOpenedItem(prevState => prevState === index ? false : index)
+  }
+
+  return (
+    <div className={classNames(
+      'accordion__item',
+      { 'accordion__item--expanded': isOpen },
+      className,
+    )}>
+      <div className='accordion__title' onClick={handleClick}>
+        <span>{title}</span> {isOpen && <Icon className='accordion__icon' type='X' />}
+      </div>
+      {isOpen &&
+        <div className='accordion__content'>{children}</div>
+      }
+    </div>
+  )
+}
 
 AccordionItem.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.node,
 }
